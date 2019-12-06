e525a966e96945fd66f82f63ac44b3724c8501bb
Bart Vandeputte <bart.vdputte@gmail.com>
2019-12-06 11:25:23 +0100

    Add composer support

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 include __DIR__ . DS . 'src' . DS . 'bettersearch.php';
 
+// For composer
+@include_once __DIR__ . '/vendor/autoload.php';
+
 Kirby::plugin('bvdputte/kirby-bettersearch', [
     'pagesMethods' => [
         'bettersearch' => function ($query, $params = array()) {
