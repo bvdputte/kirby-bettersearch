bd2f35236560a3f47b36bffb3b6a218ad0843912
Bart Vandeputte <mail@bartvandeputte.be>
2023-02-04 17:18:24 +0100

    PHP 8.2 compat + merge back parts from core search functionality

--- src/bettersearch.php
+++ src/bettersearch.php
@@ -1,50 +1,83 @@
 <?php
 /**
-* Fork of the native $pages->search() method
+* Fork of the native search() component
 * Only searches for full matches
 */
-function search($collection, $query, $params = array()) {
-
-    if(is_string($params)) {
-        $params = array('fields' => str::split($params, '|'));
+function search(Kirby\Cms\App $kirby, Kirby\Cms\Collection $collection, string $query = null, $params = []) {
+    // empty search query
+    if (empty(trim($query ?? '')) === true) {
+        return $collection->limit(0);
     }
 
-    $defaults = array(
-        'minlength' => 2,
-        'fields'    => array(),
-        'words'     => false,
-        'score'     => array()
-    );
+    if (is_string($params) === true) {
+        $params = ['fields' => Str::split($params, '|')];
+    }
 
-    $options = array_merge($defaults, $params);
+    $defaults = [
+        'fields'    => [],
+        'score'     => []
+    ];
 
+    $options     = array_merge($defaults, $params);
+    $collection  = clone $collection;
+
+    // returns an empty collection if there is no search query
     if(empty($query)) return $collection->limit(0);
 
-    $results = $collection->filter(function($page) use($query, $options) {
-        $data = $page->content()->toArray();
-        $keys = array_keys($data);
+    $scores  = [];
+    $results = $collection->filter(function ($item) use ($query, $options, &$scores) {
+        $data   = $item->content()->toArray();
+        $keys   = array_keys($data);
+        $keys[] = 'id';
 
-        if(!empty($options['fields'])) {
-            $keys = array_intersect($keys, $options['fields']);
+        if ($item instanceof User) {
+            $keys[] = 'name';
+            $keys[] = 'email';
+            $keys[] = 'role';
+        } elseif ($item instanceof Page) {
+            // apply the default score for pages
+            $options['score'] = array_merge([
+                'id'    => 64,
+                'title' => 64,
+            ], $options['score']);
         }
 
-        $page->searchHits  = 0;
-        $page->searchScore = 0;
+        if (empty($options['fields']) === false) {
+            $fields = array_map('strtolower', $options['fields']);
+            $keys   = array_intersect($keys, $fields);
+        }
+        // var_dump($keys);
 
-        foreach($keys as $key) {
-            $score = a::get($options['score'], $key, 1);
+        $scoring = [
+            'hits'  => 0,
+            'score' => 0
+        ];
 
-            // check for full matches
-            if($matches = preg_match_all('!' . preg_quote($query) . '!i', $data[$key], $r)) {
-                $page->searchHits  += $matches;
-                $page->searchScore += $matches * $score;
+        foreach ($keys as $key) {
+            $score = $options['score'][$key] ?? 1;
+            $value = $data[$key] ?? (string)$item->$key();
+
+            $lowerValue = Str::lower($value);
+
+            // check for exact query matches
+            if ($matches = preg_match_all('!' . preg_quote($query) . '!i', $value, $r)) {
+                $scoring['score'] += 2 * $score;
+                $scoring['hits']  += $matches;
             }
+
+            // check for any match
+            // if ($matches = preg_match_all($preg, $value, $r)) {
+            //     $scoring['score'] += $matches * $score;
+            //     $scoring['hits']  += $matches;
+            // }
         }
 
-        return $page->searchHits > 0 ? true : false;
+        $scores[$item->id()] = $scoring;
+        return $scoring['hits'] > 0;
     });
 
-    $results = $results->sortBy('searchScore', SORT_DESC);
-
-    return $results;
+    return $results->sort(
+        fn ($item) => $scores[$item->id()]['score'],
+        'desc'
+    );
 }
